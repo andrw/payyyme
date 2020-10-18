1a1dd1a44930949b05009acf5f39d00f5baf85c2
Andrew Yang <androo.yang@gmail.com>
2020-10-18 01:10:21 -0700

    added material-ui as a ui framework and imported some cards. set up routing in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -24,3 +24,40 @@ function App() {
 }
 
 export default App;
+*/
+
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Container from '@material-ui/core/Container';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import SimpleCard from './SimpleCard';
+import MediaCard from './MediaCard';
+import {
+  Route,
+  NavLink,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    // Anything in the UI here is rendered for ALL pages.
+    <React.StrictMode>
+    <BrowserRouter>
+    {/* This creates the server connection. */}
+    <Route path="/SimpleCard" component={SimpleCard}/> 
+    </BrowserRouter>
+    <CssBaseline />
+    <Container maxWidth="lg">
+      <MediaCard />
+    </Container>
+    </React.StrictMode>
+  );
+}
+
